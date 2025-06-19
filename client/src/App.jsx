0feb6ad0f7609a8e679db0d621af18d689f6a3e1
Sakshi Singh <130206553+sakshiji-here@users.noreply.github.com>
2025-06-19 22:09:26 +0530

    Completed UI of Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Favorite from "./pages/Favorite";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const isAdminRoute = useLocation().pathname.startsWith("./admin");
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
 
   return (
     <>
@@ -19,8 +19,8 @@ const App = () => {
       {!isAdminRoute && <NavBar />}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mpvies" element={<Movies />} />
-        <Route path="/movies:id" element={<MovieDetails />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/favorite" element={<Favorite />} />
