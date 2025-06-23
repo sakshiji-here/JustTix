72519300f3ea889dc17209afeeebbb5f9c56d71d
Sakshi Singh <130206553+sakshiji-here@users.noreply.github.com>
2025-06-23 20:13:25 +0530

    additional commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+
+const app = express();
+const port = 3000;
+
+await connectDB();
+
+//MiddleWare
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+//API Routes
+app.get("/", (req, res) => res.send("Server is Live!"));
+
+app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}`)
+);
