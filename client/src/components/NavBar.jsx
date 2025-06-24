5ba962730adc315ffa3a4d0a667e79929979f646
Sakshi Singh <130206553+sakshiji-here@users.noreply.github.com>
2025-06-24 18:57:10 +0530

    new commit

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
       <Link to="/" className="max-md:flex-1">
-        <img src={assets.logo} className="w-36 h-auto" />
+        <img src={assets.logo} className="w-45 h-18" />
       </Link>
 
       <div
