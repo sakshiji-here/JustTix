0916946f2dabc5d59b10c772731603c9267d05fb
Vishal Kumar <141834992+iamkrvishal07@users.noreply.github.com>
2025-06-28 10:27:25 +0530

    Update Movie.js

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const movieSchema = new mongoose.Schema(
   {
-    _id: { type: Number, required: true },
+    _id: { type: String, required: true },
     title: { type: String, required: true },
     overview: { type: String, required: true },
     poster_path: { type: String, required: true },
